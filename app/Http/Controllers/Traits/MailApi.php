82a0de249486f41a5875e273fa0e908e76a1a887
shibatanaoto <naoto.shibata510@gmail.com>
2015-11-28 16:29:49 +0900

    api

--- app/Http/Controllers/Traits/MailApi.php
+++ app/Http/Controllers/Traits/MailApi.php
@@ -5,13 +5,18 @@
  * Date: 15/06/21
  * Time: 18:05
  */
+
 namespace App\Http\Controllers\Traits;
+
+
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Requests\PublishClerkAccountRequest;
 use App\Http\Requests\EditShopProfileRequest;
 use Mail;
 use App\Models\Image;
+use Storage;
+
 trait MailApi
 {
     /**
@@ -22,8 +27,8 @@ trait MailApi
     public function send_mail(Request $r){
         //$message = view('mails.registrationForClerk', compact($email, $password));
         $path = storage_path()."/app/".$r->input('path');
-        $datetime = date("Y-m-d H:i:s");
-        $base64_img = base64_encode(file_get_contents($path));
+        $datetime = date("Y年m月d日 H時i分");
+        $base64_img = base64_encode(Storage::get($r->input('path')));
         $messages = <<<EOT
 {
   "image": $base64_img,
@@ -31,7 +36,8 @@ trait MailApi
 }
 EOT;
         echo $messages;
-        Image::create(['path' => $r->input('path'), 'date' => $datetime]);
+        Storage::delete($r->input('path'));
+        Image::create(['token' => $base64_img, 'date' => $datetime]);
         Mail::raw($messages, function($message)
        {
            $message->from('naoto.shibata510@gmail.com');
